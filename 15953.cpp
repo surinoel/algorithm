eb8bebf9ac8438e436ccfb73e511a5813aa38d13
surinoel <yyj48921@gmail.com>
2019-03-29 03:30:47 +0900

    5

---
+++ 15953.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int a_m[6] = { 5000000, 3000000, 2000000, 500000, 300000, 100000 };
+int b_m[5] = { 5120000, 2560000, 1280000, 640000, 320000 };
+int a[101];
+int b[101];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int idx = 1;
+	for (int i = 1; i <= 6; i++) {
+		for (int j = idx; j < idx + i; j++) {
+			a[j] = a_m[i - 1];
+		}
+		idx += i;
+	}
+
+	idx = 1;
+	for (int i = 1; i <= 5; i++) {
+		int tmp = idx << 1;
+		for (int j = idx; j < idx ; j++) {
+			b[j] = b_m[i - 1];
+		}
+		idx <<= 1;
+	}
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int x, y;
+		cin >> x >> y;
+		cout << a[x] + b[y] << '\n';
+	}
+
+	return 0;
+}
---
+++ 15954.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	
+	long double ans = 2147483647;
+	for (int i = 0; i < n; i++) {
+		vector<int> q;
+		int tmp = 0;
+		for (int j = i; j < n; j++) {
+			q.push_back(v[j]);
+			tmp += v[j];
+			if (q.size() < k) {
+				continue;
+			}
+
+			long double avg = tmp / (long double)q.size();
+			long double sum = 0;
+
+			for (int u = i; u < j + 1; u++) {
+				sum += (v[u] - avg) * (v[u] - avg);
+			}
+			sum /= (long double)q.size();
+			sum = sqrt(sum);
+			if (ans > sum) {
+				ans = sum;
+			}
+		}
+	}
+	
+	cout << fixed << setprecision(10) << ans << '\n';
+	return 0;
+}
