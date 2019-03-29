b88b64d24fbdbbc825b97f0bb5d0b2cce8b22b35
surinoel <48958763+surinoel@users.noreply.github.com>
2019-03-30 03:26:28 +0900

    Add files via upload

---
+++ 2869.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	freopen("data.txt", "r", stdin);
+	int a, b, v;
+	cin >> a >> b >> v;
+	int d = a - b;
+	int u = v - a;
+	if (u <= 0) {
+		cout << "1\n";
+	}
+	else {
+		if (u%d == 0) {
+			cout << u / d + 1 << '\n';
+		}
+		else {
+			cout << u / d + 2 << '\n';
+		}
+	}
+	return 0;
+}
