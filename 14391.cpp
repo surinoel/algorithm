ef6a926be403ce57907100765a2b1ee0a80bfb75
surinoel <48958763+surinoel@users.noreply.github.com>
2019-04-07 12:18:36 +0900

    Add files via upload

---
+++ 14391.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int mat[4][4];
+
+int main(void) {
+	freopen("data.txt", "r", stdin);
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf("%1d", &mat[i][j]);
+		}
+	}
+	int ans = 0;
+	for (int s = 0; s < (1 << n*m); s++) {
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			int cur = 0;
+			for (int j = 0; j < m; j++) {
+				int k = i*m + j;
+				if (s & (1 << k)) {
+					cur = cur * 10 + mat[i][j];
+				}
+				else {
+					sum += cur;
+					cur = 0;
+				}
+			}
+			sum += cur;
+		}
+
+		for (int j = 0; j < m; j++) {
+			int cur = 0;
+			for (int i = 0; i < n; i++) {
+				int k = i*m + j;
+				if (!(s & (1 << k))) {
+					cur = cur * 10 + mat[i][j];
+				}
+				else {
+					sum += cur;
+					cur = 0;
+				}
+			}
+			sum += cur;
+		}
+		ans = max(ans, sum);
+	}
+	cout << ans << '\n';
+	return 0;
+}
