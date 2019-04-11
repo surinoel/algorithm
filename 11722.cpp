f643196991a11e880271be87eb55167c77847a04
surinoel <48958763+surinoel@users.noreply.github.com>
2019-04-11 16:33:02 +0900

    Add files via upload

---
+++ 11722.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int a[1000];
+int d[1000];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		d[i] = 1;
+	}
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (a[i] < a[j] && d[i] < d[j] + 1) {
+				d[i] = d[j] + 1;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (d[i] > ans) {
+			ans = d[i];
+		}
+	}
+	cout << ans << '\n';
+	return 0;
+}
