aaf494eb7ff16793a2e39dd745832b9282ab5ac6
surinoel <48958763+surinoel@users.noreply.github.com>
2019-04-30 17:01:00 +0900

    Create 9020.cpp

---
+++ 9020.cpp
@@ -0,0 +1,35 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+bool prime[10001];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	memset(prime, true, sizeof(prime));
+	prime[1] = false;
+	for (int i = 2; i <= 10000; i++) {
+		if (!prime[i]) continue;
+		for (int j = i + i; j <= 10000; j += i) {
+			prime[j] = false;
+		}
+	}
+
+	int tc; cin >> tc;
+	while (tc--) {
+		int n; cin >> n;
+		int a, b;
+		for (int i = 2; i <= n / 2; i++) {
+			if (!prime[i]) continue;
+			if (!prime[n - i]) continue;
+			a = i;
+			b = n - i;
+		}
+		cout << a << ' ' << b << '\n';
+	}
+
+	return 0;
+}
