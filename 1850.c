e3ac093faf4686e3092c825f171978c56af7a0ca
surinoel <48958763+surinoel@users.noreply.github.com>
2019-05-10 19:45:09 +0900

    Create 1850.c

---
+++ 1850.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define max(n, m) (n>=m) ? n : m
+#define min(n, m) (n>=m) ? m : n
+
+long long gcd(long long n, long long m) {
+	if (m == 0) {
+		return n;
+	}
+	return gcd(m, n%m);
+}
+
+int main(void) {
+	long long n, m;
+	long long maxv, minv;
+	scanf("%lld %lld", &n, &m);
+	
+	for (int i = 0; i < gcd(max(n, m), min(n, m)); i++) {
+		printf("%d", 1);
+	}
+	printf("\n");
+	return 0;
+}
