53932bfcc4966e31718ae86715b4a67b68b75063
surinoel <48958763+surinoel@users.noreply.github.com>
2019-05-11 14:09:25 +0900

    Create 2252.cpp

---
+++ 2252.cpp
@@ -0,0 +1,53 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> st[32001];
+int degree[32001];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	freopen("data.txt", "r", stdin);
+	int n, m;
+	cin >> n >> m;
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		degree[b]++;
+		st[a].push_back(b);
+	}
+	vector<int> ans;
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		if (degree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (q.empty()) {
+			exit(1);
+		}
+		int p = q.front();
+		q.pop();
+		ans.push_back(p);
+
+		for (int k = 0; k < st[p].size(); k++) {
+			int y = st[p][k];
+			if (--degree[y] == 0) {
+				q.push(y);
+			}
+		}
+	}
+
+	for (auto x : ans) {
+		cout << x << ' ';
+	}
+	cout << '\n';
+
+	return 0;
+}
