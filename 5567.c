23ccf21e277042394f6cb1e1b70df9c2ee55432d
surinoel <48958763+surinoel@users.noreply.github.com>
2019-05-26 16:33:26 +0900

    Update 5567.c

--- 5567.c
+++ 5567.c
@@ -7,26 +7,26 @@ int main(void) {
 	int n, m;
 	scanf("%d %d", &n, &m);
 	while (m--) {
-		int x, y;
-		scanf("%d %d", &x, &y);
-		d[x][y] = 1;
-		d[y][x] = 1;
+	int x, y;
+	scanf("%d %d", &x, &y);
+	d[x][y] = 1;
+	d[y][x] = 1;
 	}
 
 	int ans = 0;
-    visit[1] = 1;
+	visit[1] = 1;
 	for (int i = 1; i <= n; i++) {
 		if (d[1][i] == 1) {
-            if(!visit[i]){
-                ans += 1;
-                visit[i] = 1;
-            }
+			if(!visit[i]) {
+				ans += 1;
+				visit[i] = 1;
+			}
 			for (int j = 1; j <= n; j++) {
 				if (d[i][j] == 1) {
-                    if(!visit[j]){
-                        ans += 1;
-                        visit[j] = 1;
-                    }
+					if(!visit[j]) {
+						ans += 1;
+						visit[j] = 1;
+					}
 				}
 			}
 		}
