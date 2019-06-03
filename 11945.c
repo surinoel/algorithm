c2dadea8fb76aefe7696f0eba5037b8f5575a3c6
surinoel <48958763+surinoel@users.noreply.github.com>
2019-06-04 01:40:52 +0900

    Create 11945.c

---
+++ 11945.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+char mat[10][10];
+
+int main(void) {
+	int n, m;
+	char c;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf(" %c ", &mat[i][j]);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = m - 1; j >= 0; j--) {
+			printf("%c", mat[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
