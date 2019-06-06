49dfa428e2015e4683876a095e1443e6d24c6b2c
surinoel <48958763+surinoel@users.noreply.github.com>
2019-06-06 17:04:11 +0900

    Update 17213.cpp

--- 17213.cpp
+++ 17213.cpp
@@ -1,3 +1,39 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+double memo[51];
+
+double fact(int n) {
+	if (n == 1 || n == 0) return 1;
+	if (memo[n] > 0) {
+		return memo[n];
+	}
+	return memo[n] = n * fact(n - 1);
+}
+
+double comb(int n, int r) {
+	double a, b, c;
+	a = fact(n);
+	b = fact(r);
+	c = fact(n - r);
+
+	return a / (b*c);
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int n, m, r;
+	cin >> n >> m;
+	r = m - n;
+	cout << fixed << setprecision(0) << comb(n + r - 1, r) << '\n';
+	return 0;
+}
+
+#if 0
 #include <iostream>
 
 using namespace std;
@@ -33,3 +69,4 @@ int main(void) {
 	cout << ans << '\n';
 	return 0;
 }
+#endif
