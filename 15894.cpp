8638dd3be9c6df4bb9b289d007f6151af25b98f6
surinoel <48958763+surinoel@users.noreply.github.com>
2019-07-28 13:13:11 +0900

    Create 15894.cpp

---
+++ 15894.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long n;
+	cin >> n;
+	cout << 4 * n << '\n';
+	return 0;
+}
