2e714d0c07dd854ea40aef3246e5965e8670cb56
surinoel <48958763+surinoel@users.noreply.github.com>
2019-08-31 14:56:06 +0900

    Create 15655.cpp

---
+++ 15655.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+bool visit[9];
+vector<int> num;
+
+void go(int idx, int maxcnt, vector<int> v) {
+	if (maxcnt == m) {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx + 1; i < n; i++) {
+		if (!visit[i]) {
+			visit[i] = true;
+			v.push_back(num[i]);
+			go(i, maxcnt + 1, v);
+			v.pop_back();
+			visit[i] = false;
+		}
+	}
+	return;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+	num.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num.begin(), num.end());
+
+	go(-1, 0, vector<int>());
+	return 0;
+}
