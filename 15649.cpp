0786f3ffac2f5a6c174c1d6d492cb7e09f368176
surinoel <48958763+surinoel@users.noreply.github.com>
2019-08-31 14:29:17 +0900

    Create 15649.cpp

---
+++ 15649.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+bool visit[9];
+
+void go(int maxcnt, vector<int> v) {
+	if (maxcnt == m) {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!visit[i]) {
+			visit[i] = true;
+			v.push_back(i);
+			go(maxcnt + 1, v);
+			v.pop_back();
+			visit[i] = false;
+		}
+	}
+	return;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+	go(0, vector<int>());
+	return 0;
+}
