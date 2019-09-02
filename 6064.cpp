cc61a5eca47b0e00b4f854e5b3fc1039c3f13a3d
surinoel <48958763+surinoel@users.noreply.github.com>
2019-09-02 12:53:38 +0900

    Create 6064.cpp

---
+++ 6064.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		int n, m, x, y;
+		cin >> n >> m >> x >> y;
+		int limit = (n * m) / gcd(max(n, m), min(n, m));
+		int target = min(x, y);
+		int ans;
+		bool ok = false;
+		if (n == x && m == y) {
+			ans = limit;
+			ok = true;
+		}
+		else {
+			if (target == x) {
+				if (x == n) x = 0;
+				if (y == m) y = 0;
+				for (int i = target; i <= limit; i += n) {
+					if (i % n == x && i % m == y) {
+						ok = true;
+						ans = i;
+						break;
+					}
+				}
+			}
+			else {
+				if (x == n) x = 0;
+				if (y == m) y = 0;
+				for (int i = target; i <= limit; i += m) {
+					if (i % n == x && i % m == y) {
+						ok = true;
+						ans = i;
+						break;
+					}
+				}
+			}
+		}
+
+		if (!ok) {
+			cout << -1 << '\n';
+		}
+		else {
+			cout << ans << '\n';
+		}
+	}
+
+	return 0;
+}
