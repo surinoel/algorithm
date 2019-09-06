aba4f23aee33a95480495b05b5953061505fd4f0
surinoel <48958763+surinoel@users.noreply.github.com>
2019-09-06 21:38:33 +0900

    Update 16988.cpp

--- 16988.cpp
+++ 16988.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
-#include <vector>
 #include <queue>
-#include <tuple>
+#include <vector>
+#include <cstring>
+#include <utility>
+#include <iostream>
 
 using namespace std;
 
+int n, m;
+int mat[20][20];
+int check[20][20][20][20];
+
+int group[20][20];
+int group_size[20 * 20 + 1];
+
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
-bool check[20][20][20][20];
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -15,7 +22,6 @@ int main(void) {
 
 	int n, m;
 	cin >> n >> m;
-	vector<vector<int>> mat(n, vector<int>(m));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> mat[i][j];
@@ -23,62 +29,65 @@ int main(void) {
 	}
 
 	int ans = 0;
-	for (int x1 = 0; x1 < n; x1++) {
-		for (int y1 = 0; y1 < m; y1++) {
-			if (mat[x1][y1] != 0) continue;
-			for (int x2 = 0; x2 < n; x2++) {
-				for (int y2 = 0; y2 < m; y2++) {
-					if (mat[x2][y2] != 0 || (x1 == x2 && y1 == y2)) continue;
-					if (check[x1][y1][x2][y2] || check[x2][y2][x1][y1]) continue;
-					check[x1][y1][x2][y2] = true;
-					check[x2][y2][x1][y1] = true;
+	for (int u1 = 0; u1 < n; u1++) {
+		for (int v1 = 0; v1 < m; v1++) {
+			if (mat[u1][v1] != 0) continue;
+			for (int u2 = 0; u2 < n; u2++) {
+				for (int v2 = 0; v2 < m; v2++) {
+					if (mat[u2][v2] != 0) continue;
+					if (u1 == u2 && v1 == v2) continue;
+					if (check[u1][v1][u2][v2] || check[u2][v2][u1][v1]) continue;
+					check[u1][v1][u2][v2] = check[u2][v2][u1][v1] = true;
 
-					vector<vector<int>> tmp(mat);
-					vector<vector<int>> group(n, vector<int>(m, -1));
-					tmp[x1][y1] = tmp[x2][y2] = 1;
-
-					int group_num = 0;
-					int sum = 0;
-					for (int i = 0; i < n; i++) {
-						for (int j = 0; j < m; j++) {
-							if (mat[i][j] != 2 || group[i][j] != -1) continue;
-							queue<pair<int, int>> q;
-							int group_size = 0;
-							bool ok = true;
-							q.push(make_pair(i, j));
-							group[i][j] = group_num;
-							group_size++;
-
-							while (!q.empty()) {
-								int x, y;
-								tie(x, y) = q.front();
-								q.pop();
-
-								for (int k = 0; k < 4; k++) {
-									int tx = x + dx[k];
-									int ty = y + dy[k];
-									if (tx < 0 || ty < 0 || tx > n - 1 || ty > m - 1) continue;
-									if (tmp[tx][ty] == 1 || group[tx][ty] != -1) continue;
-									if (tmp[tx][ty] == 0) {					
-										ok = false;
-										continue;
-									}
-									group[tx][ty] = group_num;
-									group_size++;
-									q.push(make_pair(tx, ty));
-								}
-							}
-
-
-							if (ok) {
-								sum += group_size;
-							}
-
-							group_num++;
+					int tmat[20][20];
+					for (int i = 0; i < 20; i++) {
+						for (int j = 0; j < 20; j++) {
+							tmat[i][j] = mat[i][j];
 						}
 					}
-					if (sum > ans) {
-						ans = sum;
+					tmat[u1][v1] = tmat[u2][v2] = 1;
+					
+					int tsum = 0;
+					int group_num = 0;
+					memset(group, 0, sizeof(group));
+					memset(group_size, 0, sizeof(group_size));
+					for (int i = 0; i < n; i++) {
+						for (int j = 0; j < m; j++) {
+							if (tmat[i][j] == 2 && group[i][j] == 0) {
+								
+								group[i][j] = ++group_num;
+								queue<pair<int, int>> q;
+								q.push(make_pair(i, j));
+								group_size[group_num] = 1;
+								bool ok = true;
+								while (!q.empty()) {
+									int x, y;
+									x = q.front().first;
+									y = q.front().second;
+									q.pop();
+
+									for (int k = 0; k < 4; k++) {
+										int tx = x + dx[k];
+										int ty = y + dy[k];
+
+										if (tx < 0 || ty < 0 || tx > n - 1 || ty > m - 1 || tmat[tx][ty] == 1 || group[tx][ty] != 0) continue;
+										if (tmat[tx][ty] == 0) {
+											
+											ok = false;
+										}
+										group[tx][ty] = group_num;
+										group_size[group_num] += 1;
+										q.push(make_pair(tx, ty));
+									}
+								}
+								if (ok) {
+									tsum += group_size[group_num];
+								}
+							}
+						}
+					}
+					if (tsum > ans) {
+						ans = tsum;
 					}
 				}
 			}
