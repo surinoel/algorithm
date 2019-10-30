22278e96e6f86845cff7883beb207d432dd7077d
surinoel <48958763+surinoel@users.noreply.github.com>
2019-10-30 16:02:36 +0900

    Update 2042.cpp

--- 2042.cpp
+++ 2042.cpp
@@ -4,78 +4,78 @@
 using namespace std;
 
 typedef long long ll;
-int arr[1000000];
-ll* tree;
+int *arr;
+ll *tree;
 
-long long init(int idx, int start, int end) {
-	if (start == end) {
-		return tree[idx] = arr[start];
-	}
-	else {
-		return tree[idx] = init(2 * idx + 1, start, (start + end) / 2) +
-			init(2 * idx + 2, (start + end) / 2 + 1, end);
-	}
+ll init(int idx, int start, int end) {
+    if(start == end) {
+        return tree[idx] = arr[start];
+    }
+    else {
+        return tree[idx] = init(idx*2 + 1, start, (start + end)/2) +
+            init(idx*2 + 2, (start+end)/2 + 1, end);
+    }
 }
 
-long long sum(int idx, int start, int end, int left, int right) {
-	if (left > end || right < start) {
-		return 0;
-	}
-	else if (left <= start && end <= right) {
-		return tree[idx];
-	}
-	else {
-		int mid = (start + end) / 2;
-		return sum(2 * idx + 1, start, mid, left, right) + 
-			sum(2 * idx + 2, mid + 1, end, left, right);
-	}
+ll sum(int idx, int start, int end, int left, int right) {
+    if(start > right || left > end) return 0;
+    else if(left <= start && end <= right) return tree[idx];
+    else {
+        int mid = (start + end) / 2;
+        return sum(idx*2 + 1, start, mid, left, right) +
+            sum(idx*2 + 2, mid + 1, end, left, right);
+    }
 }
 
-void change(int idx, ll diff, int start, int end, int changeidx) {
-	if (start > changeidx || end < changeidx) return;
-
-	tree[idx] += diff;
-	if (start < end) {
-		int mid = (start + end) / 2;
-		change(2 * idx + 1, diff, start, mid, changeidx);
-		change(2 * idx + 2, diff, mid + 1, end, changeidx);
-	}
+void change(int idx, int start, int end, ll diff, int changeidx) {
+    if(changeidx < start || changeidx > end) return;
+    
+    tree[idx] += diff;
+    if(start < end) {
+        int mid = (start + end) / 2;
+        change(2*idx + 1, start, mid, diff, changeidx);
+        change(2*idx + 2, mid + 1, end, diff, changeidx);
+    }
 }
 
 int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-	int n, m, k;
-	cin >> n >> m >> k;
-	int h = (int)ceil(log2(n));
-	int tree_size = 1 << (h + 1);
+    int n, m, k;
+    cin >> n >> m >> k;
+    arr = new int[n];
 
-	tree = new ll[tree_size];
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
 
-	init(0, 0, n - 1);
-	for (int i = 0; i < m + k; i++) {
-		int a, b;
-		cin >> a >> b;
-		b -= 1;
-		if (a == 1) {
-			ll c;
-			cin >> c;
-			ll diff = c - arr[b];
-			change(0, diff, 0, n - 1, b);
-			arr[b] = c;
- 		}
-		else {
-			int c;
-			cin >> c;
-			c -= 1;
-			cout << sum(0, 0, n - 1, b, c) << '\n';
-		}
-	}
+    int h = (int)ceil(log2(n));
+    int tree_size = 1 << (h + 1);
 
-	delete[] tree;
-	return 0;
+    tree = new ll[tree_size];
+    
+    init(0, 0, n-1);
+    for(int i=0; i<m+k; i++) {
+        int a;
+        ll b, c;
+        cin >> a >> b;
+        b -= 1;
+        if(a==1) {
+            cin >> c;
+            ll diff = c - arr[b];
+            change(0, 0, n-1, diff, b);
+            arr[b] = c;
+        } 
+        else {
+            cin >> c;
+            c -= 1;
+            cout << sum(0, 0, n-1, b, c) << '\n';
+        }       
+    }
+
+    delete[] arr;
+    delete[] tree;
+
+    return 0;
 }
