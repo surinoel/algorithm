e57f6119dbc50349a7ec5aa8d68986e4afe3040a
surinoel <48958763+surinoel@users.noreply.github.com>
2019-10-31 00:17:07 +0900

    Create 11758.cpp

---
+++ 11758.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long x1, x2, x3, y1, y2, y3;
+	cin >> x1 >> y1;
+	cin >> x2 >> y2;
+	cin >> x3 >> y3;
+
+	long long s = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+	if (s < 0) {
+		cout << -1 << '\n';
+	}
+	else if (s == 0) {
+		cout << 0 << '\n';
+	}
+	else {
+		cout << 1 << '\n';
+	}
+
+	return 0;
+}
