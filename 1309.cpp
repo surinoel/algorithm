bcf11e32549aa8022c5840a1137279a6a9f8f411
surinoel <48958763+surinoel@users.noreply.github.com>
2019-11-15 23:51:14 +0900

    Create 1309.cpp

---
+++ 1309.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int d[100000][3];
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	
+	d[1][0] = d[1][1] = d[1][2] = 1;
+	for (int i = 2; i <= n; i++) {
+		d[i][0] = (d[i - 1][0] + d[i - 1][1] + d[i - 1][2]) % 9901;
+		d[i][1] = (d[i - 1][0] + d[i - 1][2]) % 9901;
+		d[i][2] = (d[i - 1][0] + d[i - 1][1]) % 9901;
+	}
+
+	cout << (d[n][0] + d[n][1] + d[n][2]) % 9901 << '\n';
+	return 0;
+}
